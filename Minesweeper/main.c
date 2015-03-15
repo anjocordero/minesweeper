a339579355f3204fbf52c7807cfff560ef044616
Anjo Cordero <anjocordero@resnet-103-177.ucdavis.edu>
2015-03-15 01:22:27 -0700

    Initial Commit

---
+++ Minesweeper/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Minesweeper
+//
+//  Created by Anjo Cordero on 3/15/15.
+//  Copyright (c) 2015 Anjo Gabriel Cordero. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	printf("Hello, World!\n");
+    return 0;
+}
