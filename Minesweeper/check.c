f79ed960737b8f3ec96e5337f5809b5c221aebc5
Anjo Gabriel Cordero <anjocordero@gmail.com>
2015-03-15 18:56:53 -0700

    added checkMine
    added recursive check for mines in current tile and surrounding ones

--- Minesweeper/check.c
+++ Minesweeper/check.c
@@ -10,42 +10,89 @@ void checkMine(Board board, int playerRow, int playerCol, int* gameOver){
 		return;
 	}
 	
-	
-	if (board.tile[playerRow][playerCol + 1].is_mine == 1){ // Up
-		++board.tile[playerRow][playerCol].num_surrounding_mines;
-	}
-	if (board.tile[playerRow + 1][playerCol + 1].is_mine == 1){ // Up - Right
-		++board.tile[playerRow][playerCol].num_surrounding_mines;
-	}
-	if (board.tile[playerRow + 1][playerCol].is_mine == 1){ // Right
-		++board.tile[playerRow][playerCol].num_surrounding_mines;
-	}
-	if (board.tile[playerRow + 1][playerCol - 1].is_mine == 1){ // Down - Right
-		++board.tile[playerRow][playerCol].num_surrounding_mines;
-	}
-	if (board.tile[playerRow][playerCol - 1].is_mine == 1){ // Down
-		++board.tile[playerRow][playerCol].num_surrounding_mines;
-	}
-	if (board.tile[playerRow - 1][playerCol - 1].is_mine == 1){ // Down - Left
-		++board.tile[playerRow][playerCol].num_surrounding_mines;
-	}
-	if (board.tile[playerRow - 1][playerCol].is_mine == 1){ // Left
-		++board.tile[playerRow][playerCol].num_surrounding_mines;
-	}
-	if (board.tile[playerRow - 1][playerCol + 1].is_mine == 1){ // Up - Left
-		++board.tile[playerRow][playerCol].num_surrounding_mines;
+	if (playerRow < board.rows){
+		if (board.tile[playerRow + 1][playerCol].is_mine == 1){ // Up
+			++board.tile[playerRow][playerCol].num_surrounding_mines;
+		}
 	}
 	
+	if (playerCol < board.cols){
+		if (playerRow < board.rows){
+			if (board.tile[playerRow + 1][playerCol + 1].is_mine == 1){ // Up - Right
+				++board.tile[playerRow][playerCol].num_surrounding_mines;
+			}
+		}
+		
+		if (board.tile[playerRow][playerCol + 1].is_mine == 1){ // Right
+			++board.tile[playerRow][playerCol].num_surrounding_mines;
+		}
+		
+		if (playerRow > 0){
+			if (board.tile[playerRow - 1][playerCol + 1].is_mine == 1){ // Down - Right
+				++board.tile[playerRow][playerCol].num_surrounding_mines;
+			}
+		}
+	}
+	
+	if (playerRow > 0){
+		if (board.tile[playerRow - 1][playerCol].is_mine == 1){ // Down
+			++board.tile[playerRow][playerCol].num_surrounding_mines;
+		}
+	}
+	
+	if (playerCol > 0){
+		if (playerRow > 0){
+			if (board.tile[playerRow - 1][playerCol - 1].is_mine == 1){ // Down - Left
+				++board.tile[playerRow][playerCol].num_surrounding_mines;
+			}
+		}
+		
+		if (board.tile[playerRow][playerCol - 1].is_mine == 1){ // Left
+			++board.tile[playerRow][playerCol].num_surrounding_mines;
+		}
+		
+		if (playerRow < board.rows){
+			if (board.tile[playerRow + 1][playerCol - 1].is_mine == 1){ // Up - Left
+				++board.tile[playerRow][playerCol].num_surrounding_mines;
+			}
+		}
+	}
 	board.tile[playerRow][playerCol].appearance = (char)board.tile[playerRow][playerCol].num_surrounding_mines;
 	
 	if (board.tile[playerRow][playerCol].num_surrounding_mines == 0){
-		checkMine(board, playerRow, playerCol + 1, gameOver);
-		checkMine(board, playerRow + 1, playerCol + 1, gameOver);
-		checkMine(board, playerRow + 1, playerCol, gameOver);
-		checkMine(board, playerRow + 1, playerCol - 1, gameOver);
-		checkMine(board, playerRow, playerCol - 1, gameOver);
-		checkMine(board, playerRow - 1, playerCol - 1, gameOver);
-		checkMine(board, playerRow - 1, playerCol, gameOver);
-		checkMine(board, playerRow - 1, playerCol + 1, gameOver);
+		
+		board.tile[playerRow][playerCol].appearance = '0'; // Reveal blank space
+		
+		if (playerRow < board.rows){
+			checkMine(board, playerRow + 1, playerCol, gameOver); // Up
+		}
+	}
+	
+	if (playerCol < board.cols){
+		if (playerRow < board.rows){
+			checkMine(board, playerRow + 1, playerCol + 1, gameOver); // Up - Right
+		}
+		
+		checkMine(board, playerRow, playerCol + 1, gameOver); // Right
+		
+		if (playerRow > 0){
+			checkMine(board, playerRow, playerCol, gameOver); // Down - Right
+		}
+	}
+	
+	if (playerRow > 0){
+		checkMine(board, playerRow - 1, playerCol, gameOver); // Down
+	}
+	
+	if (playerCol > 0){
+		if (playerRow > 0){
+			checkMine(board, playerRow - 1, playerCol - 1, gameOver); // Down - Left
+		}
+		
+		checkMine(board, playerRow, playerCol - 1, gameOver); // Left
+		
+		if (playerRow < board.rows){
+			checkMine(board, playerRow + 1, playerCol - 1, gameOver); // Up - Left
+		}
 	}
 }
